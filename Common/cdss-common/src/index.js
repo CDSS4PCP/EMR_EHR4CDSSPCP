4f280dd26bfbe20c6afc30ff723379030c050d85
sorliog <sorliog@clemson.edu>
2023-12-27 14:08:01 -0500

    Developing common module

--- Common/cdss-common/src/index.js
+++ Common/cdss-common/src/index.js
@@ -50,135 +50,38 @@ let endpoints = {
     }
 };
 
-
-async function medRequestToImm(resource) {
-    let imm = {};
-    imm["resourceType"] = "Immunization";
-    imm["id"] = resource.id;
-    imm["meta"] = resource.meta;
-    imm["text"] = resource.text;
-    imm["status"] = resource.status;
-    imm["patient"] = resource.subject;
-    imm["encounter"] = resource.encounter;
-    imm["authoredOn"] = resource.authoredOn;
-    let medicationReference = resource["medicationReference"]
-
-    let url = medicationReference.reference;
-    url = endpoints.general.address.replace("{{extend}}", url);
-
-    let newResource = await fetch(url, endpoints.general);
-    let medication = await newResource.json();
-
-    imm["vaccineCode"] = medication.code;
-
-    return imm;
+/**
+ *
+ * @param rule
+ * @returns {*[]}
+ */
+function getListOfExpectedParameters(rule) {
+    return [];
 }
 
-async function buildResource(resource) {
-    const referenceKeys = ["medicationReference"];
-
-    const resourceKeys = Object.keys(resource);
-
-
-    for (const referenceKey of referenceKeys) {
-        let url = resource[referenceKey].reference;
-        url = endpoints.general.address.replace("{{extend}}", url);
-
-        let newResource = await fetch(url, endpoints.general);
-        let newJson = await newResource.json();
-
-        resource[referenceKey]["reference"] = newJson;
-
+/**
+ *
+ * @param resource
+ * @param url
+ * @returns {{entry: [{resource, fullUrl}], resourceType: string}|{entry: [{resource}], resourceType: string}|*}
+ */
+function createBundle(resource, url) {
+    if (resource.resourceType !== "Bundle") {
+        if (url === null) return {resourceType: "Bundle", entry: [{resource: resource}]}
+        return {resourceType: "Bundle", entry: [{resource: resource, fullUrl: url}]}
     }
-    return resource;
+    return resource
 }
 
-async function checkRulesForPatient(patientId, rule, libraries = null) {
-
-
-    let patientUrl = window.cdss.endpoints.patientEngineRun.address.replace("{{patientId}}", patientId)
-    let patientUrlOptions = window.cdss.endpoints.patientEngineRun;
-
-    const patientResponse = await fetch(patientUrl, patientUrlOptions);
-    if (patientResponse.status !== 200) {
-        throw {
-            status: patientResponse.status,
-            statusText: patientResponse.statusText,
-            url: patientResponse.url,
-            headers: patientResponse.headers
-        };
-    }
-
-    const patientData = await patientResponse.json();
-
-
-    let medRequestUrl = window.cdss.endpoints.medicationRequestList.address.replace("{{patientId}}", patientId);
-    console.log(medRequestUrl);
-    let medRequestUrlOptions = window.cdss.endpoints.medicationRequestList;
-
-    const medRequestResponse = await fetch(medRequestUrl, medRequestUrlOptions);
-    if (medRequestResponse.status !== 200) {
-        throw {
-            status: medRequestResponse.status,
-            statusText: medRequestResponse.statusText,
-            url: medRequestResponse.url,
-            headers: medRequestResponse.headers
-        };
-    }
-
-    const medRequestData = await medRequestResponse.json();
-
-    let medicationUrl = window.cdss.endpoints.medicationList.address.replace("{{medicationId}}", "182295e3-3a1d-4f6e-a1c4-3addb9618638");
-    let medicationUrlOptions = window.cdss.endpoints.medicationList;
-
-    const medicationResponse = await fetch(medicationUrl, medicationUrlOptions);
-    if (medicationResponse.status !== 200) {
-        throw {
-            status: medicationResponse.status,
-            statusText: medicationResponse.statusText,
-            url: medicationResponse.url,
-            headers: medicationResponse.headers
-        };
-    }
-
-    const medicationData = await medicationResponse.json();
-
-
-    const codeService = new vsac.CodeService('cache', false);
-
-    const patient = createBundle(patientData, patientUrl);
-    const medRequest = createBundle(medRequestData, medRequestUrl);
-    const medication = createBundle(medicationData, medicationUrl);
-
-    patient.entry.push(medRequest);
-    patient.total = 2;
-
-    const psource = new cqlfhir.PatientSource.FHIRv401();
-
-    psource.loadBundles([patient]);
-
-    let lib;
-    if (libraries === null || libraries === undefined || libraries.length === 0) {
-        lib = new cql.Library(rule);
-
-    } else {
-        lib = new cql.Library(rule, new cql.Repository(libraries));
-    }
-
-    // let success = await codeService.ensureValueSetsInLibraryWithAPIKey(lib, true, endpoints.uml.key);
-
-
-    const executor = new cql.Executor(lib, codeService, params);
-
-
-    const result = await executor.exec(psource); // Await the execution result
-
-    return result.patientResults;
-
-
-}
-
-async function checkRules(patient, rule, libraries = null, parameters = null,) {
+/**
+ *
+ * @param patient
+ * @param rule
+ * @param libraries
+ * @param parameters
+ * @returns {Promise<any>}
+ */
+async function executeCql(patient, rule, libraries = null, parameters = null) {
 
     const fhirWrapper = cqlfhir.FHIRWrapper.FHIRv401(); // or .FHIRv102() or .FHIRv300() or .FHIRv401()
 
@@ -220,40 +123,11 @@ async function checkRules(patient, rule, libraries = null, parameters = null,) {
     const result = await executor.exec(psource); // Await the execution result
 
     return result.patientResults;
-
-
 }
 
-async function getListOfPatients() {
-    return await fetch(window.cdss.endpoints.patientList.address, window.cdss.endpoints.patientList);
-}
-
-async function getListOfMedicationRequests() {
-    return await fetch(window.cdss.endpoints.medicationRequestList.address, window.cdss.endpoints.medicationRequestList);
-}
-
-
-function createBundle(resource, url) {
-    if (resource.resourceType !== "Bundle") {
-        if (url === null) return {resourceType: "Bundle", entry: [{resource: resource}]}
-        return {resourceType: "Bundle", entry: [{resource: resource, fullUrl: url}]}
-    }
-    return resource
-}
-
-function test1() {
-    return 1;
-}
 
 global.cdss = {
-    endpoints: endpoints,
-    checkRulesForPatient: checkRulesForPatient,
-    getListOfPatients: getListOfPatients,
-    getListOfMedicationRequests: getListOfMedicationRequests,
-    checkRules: checkRules,
-    buildResource: buildResource,
-    medRequestToImm: medRequestToImm,
-    test1: test1
+    endpoints: endpoints
 };
 
 
