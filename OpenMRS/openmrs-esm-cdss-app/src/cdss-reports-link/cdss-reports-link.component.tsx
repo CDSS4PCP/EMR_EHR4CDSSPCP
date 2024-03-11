030a7104cd06422a7949af2d80a40c549b63ac6d
sorliog <sorliog@clemson.edu>
2024-03-11 17:54:02 -0400

    Fixing reports link

--- OpenMRS/openmrs-esm-cdss-app/src/cdss-reports-link/cdss-reports-link.component.tsx
+++ OpenMRS/openmrs-esm-cdss-app/src/cdss-reports-link/cdss-reports-link.component.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { ConfigurableLink } from "@openmrs/esm-framework";
-import { useTranslation } from "react-i18next";
 
 export default function CdssReportsLink() {
-  return <ConfigurableLink to={window["getOpenmrsSpaBase"] + "/cdss-reports"}>CDSS Reports</ConfigurableLink>;
+  const url = window["origin"] + window["spaBase"] + "/cdss/reports";
+
+  return <ConfigurableLink to={url}>CDSS Reports</ConfigurableLink>;
 }
