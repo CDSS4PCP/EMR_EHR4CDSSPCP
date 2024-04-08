cadbc9bfc7faf96268aa1c9c437eb6c1d06312d5
sorliog <sorliog@clemson.edu>
2024-04-08 00:04:30 -0400

    Working on recording a usage

--- OpenMRS/cdss/api/src/main/java/org/openmrs/module/cdss/api/data/CdssUsage.java
+++ OpenMRS/cdss/api/src/main/java/org/openmrs/module/cdss/api/data/CdssUsage.java
@@ -7,43 +7,42 @@ import org.openmrs.Patient;
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
-
 @Data
 @Entity(name = "cdss_usage")
 public class CdssUsage implements OpenmrsObject {
-
-    public CdssUsage(Integer id, String vaccine, Patient patient, LocalDateTime timestamp) {
-        this.id = id;
-        this.vaccine = vaccine;
-        this.patientId = patient.getPatientId();
-        this.timestamp = timestamp;
-    }
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "cdss_usage_id")
-    private Integer id;
-
-    @Column(name = "cdss_usage_vaccine", nullable = false)
-    private String vaccine;
-
-    @Column(name = "cdss_usage_patient", nullable = false, insertable = false, updatable = false)
-    private Integer patientId;
-
-
-    @Column(name = "cdss_usage_date", nullable = false)
-    private LocalDateTime timestamp;
-
-    @Column
-    private String uuid;
-
-    public CdssUsage() {
-
-    }
-
-    @Override
-    public String toString() {
-        return "EngineUsage{" + "id=" + id + ", vaccine='" + vaccine + '\'' + ", patient=" + patientId + ", timestamp=" + timestamp + '}';
-    }
-
+	
+	public CdssUsage(Integer id, String vaccine, Patient patient, LocalDateTime timestamp) {
+		this.id = id;
+		this.vaccine = vaccine;
+		this.patientId = patient.getPatientId();
+		this.timestamp = timestamp;
+	}
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "cdss_usage_id")
+	private Integer id;
+	
+	@Column(name = "cdss_usage_vaccine", nullable = false)
+	private String vaccine;
+	
+	@Column(name = "cdss_usage_patient", nullable = false, insertable = false, updatable = false)
+	private Integer patientId;
+	
+	@Column(name = "cdss_usage_date", nullable = false)
+	private LocalDateTime timestamp;
+	
+	@Column
+	private String uuid;
+	
+	public CdssUsage() {
+		
+	}
+	
+	@Override
+	public String toString() {
+		return "EngineUsage{" + "id=" + id + ", vaccine='" + vaccine + '\'' + ", patient=" + patientId + ", timestamp="
+		        + timestamp + '}';
+	}
+	
 }
--- OpenMRS/cdss/omod/src/main/java/org/openmrs/module/cdss/web/controller/ClientsideRestController.java
+++ OpenMRS/cdss/omod/src/main/java/org/openmrs/module/cdss/web/controller/ClientsideRestController.java
@@ -1,8 +1,7 @@
 package org.openmrs.module.cdss.web.controller;
 
 import org.apache.log4j.Logger;
-import org.openmrs.Patient;
-import org.openmrs.api.PatientService;
+import org.openmrs.api.context.ServiceContext;
 import org.openmrs.module.cdss.api.RuleLoggerService;
 import org.openmrs.module.cdss.api.dao.CDSSDao;
 import org.openmrs.module.cdss.api.data.CdssUsage;
@@ -14,57 +13,48 @@ import org.springframework.web.server.ResponseStatusException;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
-
-import org.openmrs.api.context.ServiceContext;
 
 @RestController
 @RequestMapping("/cdss")
 public class ClientsideRestController {
-
-    Logger log = Logger.getLogger(ClientsideRestController.class);
-
-    @Autowired
-    CDSSDao dao;
-
-    @GetMapping(path = "/rule/{ruleId}", produces = "application/json")
-    public String getRule(@PathVariable(value = "ruleId") String ruleId) {
-        String path = "cql/" + ruleId;
-        if (!ruleId.endsWith(".json")) {
-            path = path + ".json";
-        }
-        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
-        InputStream is = classloader.getResourceAsStream(path);
-
-        if (is != null) {
-            String result = new BufferedReader(new InputStreamReader(is)).lines().collect(Collectors.joining("\n"));
-
-            return result;
-        }
-        throw new ResponseStatusException(HttpStatus.NOT_FOUND, path + " not found");
-    }
-
-    @RequestMapping(path = "/usage.form", produces = "application/json", method = {RequestMethod.POST})
-    public String recordUsage() {
-
-        PatientService service = ServiceContext.getInstance().getPatientService();
-        Patient p = service.getPatient(6);
-
-        CdssUsage usage = new CdssUsage(0, "TestVac", p, LocalDateTime.now());
-
-        //		dao.saveEngineUsage(usage);
-        log.debug("Saving " + usage.toString());
-        return "Saving " + usage.toString();
-    }
-
-    @RequestMapping(path = "/usages.form", produces = "application/json", method = {RequestMethod.GET})
-    public List<CdssUsage> getUsages() {
-
-        log.debug("CDSS: getUsages()");
-        RuleLoggerService service = ServiceContext.getInstance().getService(RuleLoggerService.class);
-        List<CdssUsage> usages = service.getRuleUsages();
-        return usages;
-    }
+	
+	Logger log = Logger.getLogger(ClientsideRestController.class);
+	
+	@Autowired
+	CDSSDao dao;
+	
+	@GetMapping(path = "/rule/{ruleId}", produces = "application/json")
+	public String getRule(@PathVariable(value = "ruleId") String ruleId) {
+		String path = "cql/" + ruleId;
+		if (!ruleId.endsWith(".json")) {
+			path = path + ".json";
+		}
+		ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+		InputStream is = classloader.getResourceAsStream(path);
+		
+		if (is != null) {
+			String result = new BufferedReader(new InputStreamReader(is)).lines().collect(Collectors.joining("\n"));
+			
+			return result;
+		}
+		throw new ResponseStatusException(HttpStatus.NOT_FOUND, path + " not found");
+	}
+	
+	@RequestMapping(path = "/record-usage.form", produces = "application/json", method = { RequestMethod.POST })
+	public String recordUsage(@RequestBody CdssUsage newUsage) {
+		log.debug("Saving " + newUsage.toString());
+		
+		dao.saveEngineUsage(newUsage);
+		return "{\"status\":\"success\"}";
+	}
+	
+	@RequestMapping(path = "/usages.form", produces = "application/json", method = { RequestMethod.GET })
+	public List<CdssUsage> getUsages() {
+		
+		RuleLoggerService service = ServiceContext.getInstance().getService(RuleLoggerService.class);
+		List<CdssUsage> usages = service.getRuleUsages();
+		return usages;
+	}
 }
