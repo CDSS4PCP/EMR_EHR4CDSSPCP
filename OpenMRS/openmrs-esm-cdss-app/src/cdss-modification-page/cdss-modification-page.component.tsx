54dd38487929e8ff5668b8be8d798db711d3d19f
sorliog <sorliog@clemson.edu>
2024-06-17 11:56:53 -0400

    Working on Rule edit table

--- OpenMRS/openmrs-esm-cdss-app/src/cdss-modification-page/cdss-modification-page.component.tsx
+++ OpenMRS/openmrs-esm-cdss-app/src/cdss-modification-page/cdss-modification-page.component.tsx
@@ -1,9 +1,55 @@
 import React from "react";
+import {
+  Form,
+  Tab,
+  Table,
+  TableBody,
+  TableCell,
+  TableHeader,
+  TableRow,
+} from "@carbon/react";
 
 export const CdssModificationPage: React.FC = () => {
   return (
     <div>
       <h1>Rule Modification</h1>
+
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableCell>Rule Description</TableCell>
+            <TableCell>Operator1</TableCell>
+            <TableCell>Age1</TableCell>
+            <TableCell>Age1 Unit</TableCell>
+            <TableCell>Boolean</TableCell>
+            <TableCell>Operator2</TableCell>
+            <TableCell>Age2</TableCell>
+            <TableCell>Age2 Unit</TableCell>
+            <TableCell>Special Condition</TableCell>
+            <TableCell>Vaccine Record</TableCell>
+            <TableCell>Vaccine Alter</TableCell>
+            <TableCell>Vaccine Dose </TableCell>
+            <TableCell>Admin Min Age </TableCell>
+            <TableCell>Admin Boolean</TableCell>
+            <TableCell>Admin Max Age</TableCell>
+            <TableCell>Interval 2</TableCell>
+            <TableCell>Interval 3</TableCell>
+            <TableCell>Interval 4</TableCell>
+            <TableCell>Medical Indication 1</TableCell>
+            <TableCell>Medical Indication 2</TableCell>
+            <TableCell>Medical Indication 3</TableCell>
+            <TableCell>Action 1 </TableCell>
+            <TableCell>Action 2 </TableCell>
+            <TableCell>Action 3 </TableCell>
+            <TableCell>Action 4 </TableCell>
+            <TableCell>Note </TableCell>
+            <TableCell>Note </TableCell>
+            <TableCell> </TableCell>
+          </TableRow>
+        </TableHeader>
+
+        <TableBody></TableBody>
+      </Table>
     </div>
   );
 };
