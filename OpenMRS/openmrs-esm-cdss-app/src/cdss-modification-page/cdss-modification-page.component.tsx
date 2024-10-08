515eb99411ed8f008b10ed93dcbb842664b17382
sorliog <sorliog@clemson.edu>
2024-10-08 13:08:05 -0400

    Working on rule modification table

--- OpenMRS/openmrs-esm-cdss-app/src/cdss-modification-page/cdss-modification-page.component.tsx
+++ OpenMRS/openmrs-esm-cdss-app/src/cdss-modification-page/cdss-modification-page.component.tsx
@@ -4,51 +4,46 @@ import {
   Tab,
   Table,
   TableBody,
-  TableCell,
+  TableCell, TableHead,
   TableHeader,
-  TableRow,
+  TableRow
 } from "@carbon/react";
+import mmr from "./MMR_descriptor.json";
 
 export const CdssModificationPage: React.FC = () => {
   return (
     <div>
       <h1>Rule Modification</h1>
-
+      {/*{JSON.stringify(mmr, null, 2)}*/}
       <Table>
-        <TableHeader>
+        <TableHead>
           <TableRow>
-            <TableCell>Rule Description</TableCell>
-            <TableCell>Operator1</TableCell>
-            <TableCell>Age1</TableCell>
-            <TableCell>Age1 Unit</TableCell>
-            <TableCell>Boolean</TableCell>
-            <TableCell>Operator2</TableCell>
-            <TableCell>Age2</TableCell>
-            <TableCell>Age2 Unit</TableCell>
-            <TableCell>Special Condition</TableCell>
-            <TableCell>Vaccine Record</TableCell>
-            <TableCell>Vaccine Alter</TableCell>
-            <TableCell>Vaccine Dose </TableCell>
-            <TableCell>Admin Min Age </TableCell>
-            <TableCell>Admin Boolean</TableCell>
-            <TableCell>Admin Max Age</TableCell>
-            <TableCell>Interval 2</TableCell>
-            <TableCell>Interval 3</TableCell>
-            <TableCell>Interval 4</TableCell>
-            <TableCell>Medical Indication 1</TableCell>
-            <TableCell>Medical Indication 2</TableCell>
-            <TableCell>Medical Indication 3</TableCell>
-            <TableCell>Action 1 </TableCell>
-            <TableCell>Action 2 </TableCell>
-            <TableCell>Action 3 </TableCell>
-            <TableCell>Action 4 </TableCell>
-            <TableCell>Note </TableCell>
-            <TableCell>Note </TableCell>
-            <TableCell> </TableCell>
+            {mmr.columns.map((m) => {
+              return <TableHeader>{m}</TableHeader>;
+            })}
           </TableRow>
-        </TableHeader>
+        </TableHead>
 
-        <TableBody></TableBody>
+        <TableBody>
+          {Object.keys(mmr)
+            .filter((f) => f != "columns")
+            .map((r) => (
+              <TableRow>
+                {Object.keys(mmr[r]).map((m) => {
+                  return <TableCell>{mmr[r][m]}</TableCell>;
+                })}
+              </TableRow>
+            ))}
+
+          {/*<TableRow>*/}
+          {/*  <TableCell>*/}
+          {/*    Regular and catch-up without prior vaccination records*/}
+          {/*  </TableCell>*/}
+          {/*  <TableCell>*/}
+          {/*    sadas*/}
+          {/*  </TableCell>*/}
+          {/*</TableRow>*/}
+        </TableBody>
       </Table>
     </div>
   );
