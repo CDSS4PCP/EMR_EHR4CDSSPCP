a7c7ab6d0810c77ae1d00c77f41a16c0b27eb454
sorliog <sorliog@clemson.edu>
2024-10-28 14:10:12 -0400

    Working on rule modification table

--- OpenMRS/openmrs-esm-cdss-app/src/cdss-modification-page/cdss-modification-page.component.tsx
+++ OpenMRS/openmrs-esm-cdss-app/src/cdss-modification-page/cdss-modification-page.component.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   Form,
   Tab,
+  DataTableSkeleton,
   Table,
   TableBody,
   TableCell,
@@ -10,31 +11,81 @@ import {
   TableRow,
 } from "@carbon/react";
 import mmr from "./MMR_descriptor.json";
+import { openmrsFetch } from "@openmrs/esm-framework";
+import { use } from "i18next";
+
+async function getRuleData() {
+  const result = await openmrsFetch("/cdss/rule-manifest.form");
+  const json = await result.json();
+  return json;
+}
 
 export const CdssModificationPage: React.FC = () => {
+  const [ruleData, setRuleData] = useState(null);
+  const [columns, setColumns] = useState(null);
+  useEffect(() => {
+    getRuleData().then((r) => {
+      setRuleData(r);
+
+      const cols = [];
+      for (const rule of r.rules) {
+        const paramNames = Object.keys(rule.params);
+        for (const paramName of paramNames) {
+          const param = structuredClone(rule.params[paramName]);
+          param["name"] = paramName;
+          cols.push(param);
+        }
+      }
+      cols.sort((a, b) => a.name.localeCompare(b.name));
+      setColumns(cols);
+    });
+  });
+
+  if (ruleData == null && columns != null) {
+    return <DataTableSkeleton headers={columns} aria-label="sample table" />;
+  }
+  if (ruleData == null && columns == null) {
+    return <DataTableSkeleton headers={[]} aria-label="sample table" />;
+  }
   return (
     <div>
       <h1>Rule Modification</h1>
-      {/*{JSON.stringify(mmr, null, 2)}*/}
+
       <Table useZebraStyles>
         <TableHead>
           <TableRow>
-            {mmr.columns.map((m) => {
-              return <TableHeader>{m}</TableHeader>;
-            })}
+            <TableHeader>Name</TableHeader>
+            {columns != null &&
+              columns.map((m) => {
+                return <TableHeader>{m.name != null && m.name}</TableHeader>;
+              })}
           </TableRow>
         </TableHead>
 
         <TableBody>
-          {Object.keys(mmr)
-            .filter((f) => f != "columns")
-            .map((r) => (
-              <TableRow>
-                {Object.keys(mmr[r]).map((m) => {
-                  return <TableCell>{mmr[r][m]}</TableCell>;
-                })}
-              </TableRow>
-            ))}
+          {ruleData != null &&
+            ruleData.rules != null &&
+            ruleData.rules
+              .filter((r) => {
+                if (r.role == null) return true;
+                else return r.role.toLowerCase() == "rule";
+              })
+              .map((r) => {
+                return (
+                  <TableRow>
+                    <TableCell>{r.id}</TableCell>
+                    {columns.map((column) => {
+                      return (
+                        <TableCell>
+                          {r.params != null &&
+                            r.params[column.name] != null &&
+                            r.params[column.name].value}
+                        </TableCell>
+                      );
+                    })}
+                  </TableRow>
+                );
+              })}
         </TableBody>
       </Table>
     </div>
