138b545f530426a6a335262d03d05d3bee9d6667
sorliog <sorliog@clemson.edu>
2024-11-30 18:33:29 -0500

    Fixing rules

--- OpenMRS/cdss/api/src/main/java/org/openmrs/module/cdss/api/RuleManagerService.java
+++ OpenMRS/cdss/api/src/main/java/org/openmrs/module/cdss/api/RuleManagerService.java
@@ -15,31 +15,31 @@ import java.util.Map;
 
 public interface RuleManagerService extends CdssVaccineService {
 
-    @Authorized({CDSSConfig.MODULE_PRIVILEGE})
+//    @Authorized({CDSSConfig.MODULE_PRIVILEGE})
     List<String> getAllRules() throws APIAuthenticationException;
 
-    @Authorized({CDSSConfig.MODULE_PRIVILEGE})
+//    @Authorized({CDSSConfig.MODULE_PRIVILEGE})
     List<String> getEnabledRules() throws APIAuthenticationException;
 
     // TODO Change methods to utilize version as well as ruleId
-    @Authorized({CDSSConfig.MODULE_PRIVILEGE})
+//    @Authorized({CDSSConfig.MODULE_PRIVILEGE})
     String getElmRule(String ruleId) throws APIAuthenticationException, RuleNotFoundException, FileNotFoundException;
 
-    @Authorized({CDSSConfig.MODULE_PRIVILEGE})
+//    @Authorized({CDSSConfig.MODULE_PRIVILEGE})
     String getCqlRule(String ruleId) throws APIAuthenticationException, RuleNotFoundException, FileNotFoundException;
 
-    @Authorized({CDSSConfig.MODULE_PRIVILEGE})
+//    @Authorized({CDSSConfig.MODULE_PRIVILEGE})
     RuleManifest getRuleManifest() throws APIAuthenticationException;
 
-    @Authorized({CDSSConfig.MODULE_PRIVILEGE})
+//    @Authorized({CDSSConfig.MODULE_PRIVILEGE})
     List<String> getAllRules(RuleRole role) throws APIAuthenticationException;
 
-    @Authorized()
+//    @Authorized({CDSSConfig.MODULE_PRIVILEGE})
     Boolean enableRule(String ruleId) throws APIAuthenticationException, RuleNotFoundException, FileNotFoundException;
 
-    @Authorized()
+//    @Authorized({CDSSConfig.MODULE_PRIVILEGE})
     Boolean disableRule(String ruleId) throws APIAuthenticationException, RuleNotFoundException, FileNotFoundException;
 
-    @Authorized({CDSSConfig.MODULE_PRIVILEGE})
+//    @Authorized({CDSSConfig.MODULE_PRIVILEGE})
     Boolean modifyRule(String ruleId, String version, Map<String, ParamDescriptor> changedParameters) throws JsonProcessingException, FileNotFoundException, RuleNotFoundException;
 }
